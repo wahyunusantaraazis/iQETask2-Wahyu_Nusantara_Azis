d0007fce723bd1dedd9ef38e14289fbb877e3468
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-11-06 13:41:58 +0700

    saya add fitur 1 = fitur user

---
+++ src/com/perusahaan/User.java
@@ -0,0 +1,26 @@
+package com.perusahaan;
+
+public class User {
+    String HP;
+    String nama;
+
+    public String getHP() {
+        return HP;
+    }
+
+    public void setHP(String HP) {
+        this.HP = HP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getInfo(){
+        return "Halo Saya "+this.nama;
+    }
+}
